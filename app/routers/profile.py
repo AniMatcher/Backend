f7ff59339de682c3d335b72679ba884cc5846409
Kelly Huang <kelly.g.huang@duke.edu>
2023-10-04 22:51:18 -0400

    added get to profiles

--- app/routers/profile.py
+++ app/routers/profile.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, APIRouter, Depends, HTTPException
 from ..db.profile_crud import post_new_user
+from ..db.crud import check_user_existence
 from ..profile_schema import Profile
 #from .. import files
 
@@ -10,12 +11,18 @@ router = APIRouter(
     )
 
 @router.post("/new-user/")
-async def make_user_profile(uuid: str, username: str, gender: str, sex_pref: str, genre: str, bio: str):
+async def make_user_profile(email: str, gender: str, sex_pref: str, genre: str, bio: str):
+    matched_users = check_user_existence(email)
+    if not matched_users.data: 
+        raise HTTPException(status_code = 500, detail = "Error invalid email")
+    else:
+        uuid = matched_users.data[0]["uuid"]
+        username = matched_users.data[0]["username"]
     profile = Profile(
             uuid= uuid,
             username= username,
             gender= gender,
-            sex_pref= sex_pref, ###########################THIS NEEDS TO BE CHANGED
+            sex_pref= sex_pref, 
             genre= genre,
             bio=bio,
         )

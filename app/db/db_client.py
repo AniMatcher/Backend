5dacf6cb585d6512f99b021d674e25dd92bb4168
Philip Yi <philip.yi@duke.edu>
2023-11-04 15:53:09 -0400

    fixed dotenv stuff so we dont leak api keys

--- app/db/db_client.py
+++ app/db/db_client.py
@@ -1,8 +1,9 @@
 import os
 from supabase import create_client, Client
+from dotenv import load_dotenv
 
+load_dotenv()
 
-
-url: str = os.environ.get("SUPABASE_URL")
-key: str = os.environ.get("SUPABASE_KEY")
-client: Client = create_client("https://hghehkvmtmkxmjppybvp.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhnaGVoa3ZtdG1reG1qcHB5YnZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTYzNjcxMDMsImV4cCI6MjAxMTk0MzEwM30.wtQJPuDKo2hY2z80aZqouIURUolu1LVW6C7mFhuQlgs")
+url: str = os.getenv("SUPABASE_URL")
+key: str = os.getenv("SUPABASE_KEY")
+client: Client = create_client(url, key)

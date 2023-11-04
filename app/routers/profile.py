6cd60aa96a9fe9aba103228a05ca097fe0b89ec7
Arnav Jindal <arnav.jindal7@gmail.com>
2023-11-04 15:11:13 -0400

    new user crud

--- app/routers/profile.py
+++ app/routers/profile.py
@@ -54,13 +54,13 @@ async def make_user_profile(profile: PostProfile):
             genre= profile.gender,
             bio=profile.bio,
         )
-    response = profile_crud.post_new_user(profile = profile)
+    response = users_crud.post_new_user(profile = profile)
     if not response:
         raise HTTPException(status_code = 500, detail = "Error creating user")
     return {"message": "Profile created successfully"}
 
 @router.post("/animes/")
-async def make_user_profile(profile: UserAnimesPost):
+async def make_user_animes(profile: UserAnimesPost):
     matched_users = auth_crud.check_email_user_existence(profile.email)
     if not matched_users.data: 
         raise HTTPException(status_code = 500, detail = "Error invalid email")

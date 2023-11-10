d427f65d1e9252d49b63d7bf1623baa0645cd6de
Kelly Huang <kelly.g.huang@duke.edu>
2023-11-09 19:30:54 -0500

    edit put_edit_user

--- app/db/users_crud.py
+++ app/db/users_crud.py
@@ -25,7 +25,8 @@ def put_edit_user(profile: Profile):
         "gender": profile.gender,
         "sex_pref": profile.sex_pref,
         "genre": profile.genre,
-        "bio": profile.bio
+        "bio": profile.bio,
+        "image": profile.image
     }
     return table.update(update_data).eq("uuid", profile.uuid).execute()
 
